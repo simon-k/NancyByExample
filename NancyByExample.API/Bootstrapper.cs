e84474e6940ba5dbec1e9b119077261e4651dc97
Simon Kofod <github@kofod.it>
2017-05-17 20:18:33 +0200

    Remove error handling from bootstrap

--- NancyByExample.API/Bootstrapper.cs
+++ NancyByExample.API/Bootstrapper.cs
@@ -10,11 +10,6 @@ namespace NancyByExample.API
         protected override void ApplicationStartup(ILifetimeScope container, IPipelines pipelines)
         {
             base.ApplicationStartup(container, pipelines);
-            /*pipelines.OnError += (ctx, ex) =>
-            {
-                Console.WriteLine(string.Format("Error occured during request ('{0}')", ctx.Request.Path), ex);
-                return null;
-            };*/
         }
 
         protected override void ConfigureApplicationContainer(ILifetimeScope existingContainer)
